4a053114da851e49b6b64f26999cab092a657761
Haider <go2riz@gmail.com>
2026-07-20 13:29:23 +0500

    Revert changes

--- asg.tf
+++ asg.tf
@@ -25,12 +25,17 @@ resource "aws_autoscaling_group" "ecs" {
       }
     }
 
-    instances_distribution {
-      spot_allocation_strategy                   = "lowest-price"
-      spot_instance_pools                        = 3
-      on_demand_base_capacity                    = 0
-      on_demand_percentage_above_base_capacity   = var.on_demand_percentage
-    }
+#     instances_distribution {
+#       spot_allocation_strategy                   = "lowest-price"
+#       spot_instance_pools                        = 3
+#       on_demand_base_capacity                    = 0
+#       on_demand_percentage_above_base_capacity   = var.on_demand_percentage
+#     }
+      instances_distribution {
+        spot_allocation_strategy = "price-capacity-optimized"
+        on_demand_base_capacity = 0
+        on_demand_percentage_above_base_capacity = var.on_demand_percentage
+      }
   }
 
   vpc_zone_identifier = var.private_subnet_ids
